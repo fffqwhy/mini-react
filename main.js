626b1bb38c5a209f5840c7de207eeb448580a1d4
qyfu <599769224@qq.com>
2024-03-23 18:30:44 +0800

    将数据结构的生成交给方法来实现

--- main.js
+++ main.js
@@ -17,23 +17,65 @@
  * @param2 value 
  * @param3 childern
  */
+// const TEXT_TYPE = "TEXT_ELEMENT";
+// const TextEle = {
+//     type:TEXT_TYPE,
+//     props:{
+//         nodeValue:"test-nodeValue",
+//         children:[]
+//     }
+// }
+// const ele = {
+//     type:'div',
+//     props:{
+//         id:"app",
+//         children:[TextEle],
+//     }
+// };
+// const dom = document.createElement(ele.type);
+// dom.id = ele.props.id;
+// document.querySelector("#root").append(dom);
+// const textNode = document.createTextNode("");
+// textNode.nodeValue = TextEle.props.nodeValue;
+// dom.append(textNode);
+
+/**
+ * 数据结构需要支持动态的生成
+ */
 const TEXT_TYPE = "TEXT_ELEMENT";
-const TextEle = {
-    type:TEXT_TYPE,
-    props:{
-        nodeValue:"test-nodeValue",
-        children:[]
+/**
+ * @description 生成含有nodeValue 的数据结构
+ * @param  text 文本 
+ * @returns 含有nodeValue 的数据结构
+ */
+function createTextNode(text){
+    return {
+        type: TEXT_TYPE,
+        props: {
+            nodeValue: text,
+            children: []
+        }
     }
 }
-const ele = {
-    type:'div',
-    props:{
-        id:"app",
-        children:[TextEle],
+/**
+ * @description react 的createElement；
+ * @param {标签类型} type 
+ * @param {标签的属性} props 
+ * @param  {...any} childern 
+ */
+function createElement(type,props,...childern){
+    return {
+        type,
+        props:{
+            ...props,
+            childern,
+        }
     }
-};
-const dom = document.createElement(ele.type);
-dom.id = ele.props.id;
+}
+const TextEle = createTextNode("app_text");
+const App = createElement("div",{id:"app",class:"app"},TextEle)
+const dom = document.createElement(App.type);
+dom.id = App.props.id;
 document.querySelector("#root").append(dom);
 const textNode = document.createTextNode("");
 textNode.nodeValue = TextEle.props.nodeValue;
