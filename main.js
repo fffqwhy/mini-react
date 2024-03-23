667a0fff74954088c12d364de07bf0af77e8c1fb
qyfu <599769224@qq.com>
2024-03-23 18:18:30 +0800

    将渲染的数据提取出来作为一个数据结构

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const dom = document.createElement("div");
+// const dom = document.createElement("div");
 
-dom.id = "app";
+// dom.id = "app";
 
-document.querySelector("#root").append(dom);
+// document.querySelector("#root").append(dom);
 
-const textNode = document.createTextNode("");
+// const textNode = document.createTextNode("");
 
-textNode.nodeValue = "test text";
+// textNode.nodeValue = "test text";
 
-dom.append(textNode);
+// dom.append(textNode);
 
 /**
  * 写死的形式
@@ -17,3 +17,24 @@ dom.append(textNode);
  * @param2 value 
  * @param3 childern
  */
+const TEXT_TYPE = "TEXT_ELEMENT";
+const TextEle = {
+    type:TEXT_TYPE,
+    props:{
+        nodeValue:"test-nodeValue",
+        children:[]
+    }
+}
+const ele = {
+    type:'div',
+    props:{
+        id:"app",
+        children:[TextEle],
+    }
+};
+const dom = document.createElement(ele.type);
+dom.id = ele.props.id;
+document.querySelector("#root").append(dom);
+const textNode = document.createTextNode("");
+textNode.nodeValue = TextEle.props.nodeValue;
+dom.append(textNode);
