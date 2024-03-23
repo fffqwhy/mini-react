9cc615c513e3991f8c5436be6319533190e3ede3
qyfu <599769224@qq.com>
2024-03-23 20:18:57 +0800

    使用vite 获取了jsx,返回的标签不是一个react组件

---
+++ mini-react-v2/app.jsx
@@ -0,0 +1,9 @@
+import React from './core/React.js';
+
+// const App = React.createElement("div", { id: "app", class: "app" }, 'vite-render', "justString");
+const App = <div>
+        这是一个div
+        <h1>这是一个h1</h1>
+    </div>
+
+export default App;
---
+++ mini-react-v2/main.js
@@ -0,0 +1,4 @@
+import ReactDom from './core/ReactDom.js';
+import App from './app.jsx';
+
+ReactDom.createRoot(document.getElementById("root")).render(App);
