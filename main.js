285b9dd248a2d6fabac1ef3b094005017ca5cbee
qyfu <599769224@qq.com>
2024-03-23 19:03:48 +0800

    将渲染动作使用方法递归支持了多层dom,ps:上个版本children写错

--- main.js
+++ main.js
@@ -42,13 +42,62 @@
 /**
  * 数据结构需要支持动态的生成
  */
+// const TEXT_TYPE = "TEXT_ELEMENT";
+// /**
+//  * @description 生成含有nodeValue 的数据结构
+//  * @param  text 文本 
+//  * @returns 含有nodeValue 的数据结构
+//  */
+// function createTextNode(text) {
+//     return {
+//         type: TEXT_TYPE,
+//         props: {
+//             nodeValue: text,
+//             children: []
+//         }
+//     }
+// }
+// /**
+//  * @description react 的createElement；
+//  * @param {标签类型} type 
+//  * @param {标签的属性} props 
+//  * @param  {...any} childern 
+//  */
+// function createElement(type, props, ...childern) {
+//     return {
+//         type,
+//         props: {
+//             ...props,
+//             childern,
+//         }
+//     }
+// }
+// const TextEle = createTextNode("app_text");
+// const App = createElement("div", { id: "app", class: "app" }, TextEle)
+// const dom = document.createElement(App.type);
+// dom.id = App.props.id;
+// document.querySelector("#root").append(dom);
+// const textNode = document.createTextNode("");
+// textNode.nodeValue = TextEle.props.nodeValue;
+// dom.append(textNode);
+
+
+/**
+ * @part3  开始抽象
+ * 数据结构有了，生成数据结构的方法有了，现在要处理生成、渲染了
+ * 把大象关冰箱，总共分三步：
+ * 1、根据数据结构生成对应标签
+ * 2、根据数据结构中的props 赋给标签属性。。。
+ * 3、将生成的标签渲染
+ */
+
 const TEXT_TYPE = "TEXT_ELEMENT";
 /**
  * @description 生成含有nodeValue 的数据结构
  * @param  text 文本 
  * @returns 含有nodeValue 的数据结构
  */
-function createTextNode(text){
+function createTextNode(text) {
     return {
         type: TEXT_TYPE,
         props: {
@@ -63,20 +112,39 @@ function createTextNode(text){
  * @param {标签的属性} props 
  * @param  {...any} childern 
  */
-function createElement(type,props,...childern){
+function createElement(type, props, ...children) {
     return {
         type,
-        props:{
+        props: {
             ...props,
-            childern,
+            children,
         }
     }
 }
-const TextEle = createTextNode("app_text");
-const App = createElement("div",{id:"app",class:"app"},TextEle)
-const dom = document.createElement(App.type);
-dom.id = App.props.id;
-document.querySelector("#root").append(dom);
-const textNode = document.createTextNode("");
-textNode.nodeValue = TextEle.props.nodeValue;
-dom.append(textNode);
+/**
+ * 渲染函数
+ * @param {*} ele 数据结构 
+ * @param {*} container root 
+ */
+function render(ele, container) {
+    console.log(ele,container);
+    const dom = ele.type === TEXT_TYPE
+        ? document.createTextNode("")
+        : document.createElement(ele.type);
+    // 将除了children的属性赋给生成的dom
+    Object.keys(ele.props).forEach((propsKey)=>{
+        if(propsKey!=="children"){
+            dom[propsKey] = ele.props[propsKey];
+        }
+    })
+    // 如果有子节点，处理子节点
+    const children = ele.props.children||[];
+    children.forEach((child)=>{
+        render(child,dom);
+    })
+    container.append(dom);
+}
+const TextEle = createTextNode("render-text");
+const TextEle2 = createTextNode("render-text22");
+const App = createElement("div", { id: "app", class: "app" }, TextEle,TextEle2)
+render(App,document.getElementById("root"));
