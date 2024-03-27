e368e677ecee24eacd1b026e79551236664a2a23
qyfu <599769224@qq.com>
2024-03-27 22:43:46 +0800

    readme

--- mini-react-v2/app.jsx
+++ mini-react-v2/app.jsx
@@ -5,23 +5,8 @@ function App() {
     return <div>
         这是一个div
         <h1>这是一个h1</h1>
-        <div>div1
-            <div>div11
-                <div>div111
-                    <div style={{margin:'12px'}}>div112
-                        <div>div113
-                            <Test value={123} />
-                            <Test value={1234} />
-                            <Test value={1235} />
-                            <div>div114</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <TestTest />
-            <div>div12</div>
-
-        </div>
+        <TestTest />
+        <Test value={123} />
     </div>
 }
 
